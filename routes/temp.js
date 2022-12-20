5521be9dcb6863aa165ec071c87b1309845a75c3
pol-dev-shinroo <manus.dream.language@gmail.com>
2022-12-20 12:03:31 +0900

    bug fix -> 현재 weather api 가 인식하는 도시만 가능 -> 나중에 확장 가능

--- routes/temp.js
+++ routes/temp.js
@@ -9,42 +9,40 @@ router.get("/", async (req, res) => {
   if (errorCode !== undefined) {
     return res.status(400).json({ msg: errorCode });
   }
-  const { data: cityData } = await getTempData(dbSuccessData.city_name);
+
   // console.log("year", new Date(cityData.list[0].dt * 1000).getFullYear());
   // console.log("month", getMonth(new Date(cityData.list[0].dt * 1000)));
   // console.log("day", new Date(cityData.list[0].dt * 1000).getDate());
-
-  if (cityData.cod !== "200") {
-    return res.status(400).json({ msg: "something wrong with weather api" });
-  }
-
-  const updatedList = cityData.list.map((item) => {
-    return {
-      dt: item.dt,
-      temp: item.main.temp,
-      feels_like: item.main.feels_like,
-      humidity: item.main.humidity,
-      cloud: item.clouds.all,
-      wind_speed: item.wind.speed,
-      weather_id: item.weather[0].id,
-      weather_main: item.weather[0].main,
-      weather_desc: item.weather[0].description,
-    };
-  });
-
+  console.log(dbSuccessData.city_name);
   // db 조회
   const city = await City.findOne({ city_name: dbSuccessData.city_name });
-  console.log(city);
   // if there are no cities
   if (!city) {
+    const { data: cityData } = await getTempData(dbSuccessData.city_name);
+    if (cityData.cod !== "200") {
+      return res.status(400).json({ msg: "something wrong with weather api" });
+    }
+    const updatedList = cityData.list.map((item) => {
+      return {
+        dt: item.dt,
+        temp: item.main.temp,
+        feels_like: item.main.feels_like,
+        humidity: item.main.humidity,
+        cloud: item.clouds.all,
+        wind_speed: item.wind.speed,
+        weather_id: item.weather[0].id,
+        weather_main: item.weather[0].main,
+        weather_desc: item.weather[0].description,
+      };
+    });
     const city = await City.create({
-      city_name: cityData.city.name,
+      city_name: dbSuccessData.city_name,
       list: updatedList,
     });
     return res.status(200).json({ res: true, data: city });
+  } else {
+    return res.status(200).json({ res: true, data: city });
   }
-
-  return res.status(200).json({ res: true, data: city });
 });
 
 module.exports = router;
