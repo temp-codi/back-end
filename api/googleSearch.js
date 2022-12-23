22b95a916b9bd27e5064c891594fbe48961f60b9
pol-dev-shinroo <manus.dream.language@gmail.com>
2022-12-23 23:34:25 +0900

    구글 이미지 api 함수 모듈화

--- api/googleSearch.js
+++ api/googleSearch.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 
-const getGoogleSearchImgs = async (keywords) => {
+const getGoogleSearchImgs = async (keywords, pageNo) => {
   const response = await axios.get(`
- https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_SEARCH_ENGINE_ID}&q=${keywords}&searchType=image
+ https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_SEARCH_ENGINE_ID}&q=${keywords}&searchType=image&num=5&start=${pageNo}
  `);
   const arr = response.data.items.map((item) => {
     return item.link;
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const { testNotionDB } = require("./api/notion");
 
 // google search engine
 const { getGoogleSearchImgs } = require("./api/googleSearch");
-getGoogleSearchImgs("pants for men");
+getGoogleSearchImgs("pants for men", 1);
 
 const start = async () => {
   try {
