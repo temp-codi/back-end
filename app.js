618b84f3bf41b30156553ba87290463d6a1e8a7a
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-01-23 18:32:43 +0900

    render testing

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-const connectDB = require("./db/connect");
-const express = require("express");
-const app = express();
-const server = require("http").Server(app);
+require("dotenv").config();
+require("express-async-errors");
+const morgan = require("morgan");
+
+// extra security packages
+const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
-const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
-const packageJson = require("./package.json");
-require("dotenv").config();
 
-// Basic SetUp
-/*******************************************************/
-// logger
-const morgan = require("morgan");
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDoument = YAML.load("./swagger.yaml");
+
+const express = require("express");
+const app = express();
 app.use(morgan("tiny"));
-// allow body req
-// cors
+
+// connect DB
+const connectDB = require("./db/connect");
+
+// Routes
+const city = require("./routes/city");
+const temp = require("./routes/temp");
+const openai = require("./routes/openai");
+const google = require("./routes/google");
+
+// error handlers
+const NotFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
 app.set("trust proxy", 1); // express-rate-limit: If you are behind a proxy/load balancer (usually the case with most hosting services, e.g. Heroku, Bluemix, AWS ELB, Nginx, Cloudflare, Akamai, Fastly, Firebase Hosting, Rackspace LB, Riverbed Stingray, etc.)
 app.use(
   rateLimit({
@@ -28,26 +42,12 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// Swagger
-// const swaggerUI = require("swagger-ui-express");
-// const YAML = require("yamljs");
-// const swaggerDoument = YAML.load("./swagger.yaml");
-
 // Routes
 /*******************************************************/
-const city = require("./routes/city");
-const temp = require("./routes/temp");
-const openai = require("./routes/openai");
-const google = require("./routes/google");
-app.use(`/api/${packageJson.version}/city`, city);
-app.use(`/api/${packageJson.version}/temp`, temp);
-app.use(`/api/${packageJson.version}/openai`, openai);
-app.use(`/api/${packageJson.version}/google`, google);
-
-// error middleware
-/*******************************************************/
-const NotFound = require("./middleware/not-found");
-const errorHandlerMiddleware = require("./middleware/error-handler");
+app.use(`/api/v1/city`, city);
+app.use(`/api/v1/temp`, temp);
+app.use(`/api/v1/openai`, openai);
+app.use(`/api/v1/google`, google);
 app.use(NotFound);
 app.use(errorHandlerMiddleware);
 
@@ -57,7 +57,6 @@ app.use(errorHandlerMiddleware);
 const { getTableDB } = require("./api/notion");
 
 const useOpenGPT = require("./api/openAi");
-// useOpenGPT("stormy");
 
 const port = process.env.PORT || 3000;
 
@@ -66,7 +65,7 @@ const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
     console.log("Connected to DB");
-    server.listen(port, () => {
+    app.listen(port, () => {
       console.log("listening on port " + port);
     });
   } catch (err) {
